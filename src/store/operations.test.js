fcc72f36231a8dabc40a00cb09d28893b199e8ec
Danil <den_lad@softgrad.com>
2019-12-06 16:59:28 +0300

    operations rename

--- src/store/operations.test.js
+++ src/store/operations.test.js
@@ -9,22 +9,22 @@ it(`Operation should make a correct API call to /hotels`, () => {
   const dispatch = jest.fn();
   const api = createAPI(dispatch);
   const apiMock = new MockAdapter(api);
-  const loadAllOffers = Operation.loadAllOffers();
+  const loadOffers = Operation.loadAllOffers();
 
   apiMock
     .onGet(`/hotels`)
     .reply(200, OFFER_MOCK);
 
-  return loadAllOffers(dispatch, jest.fn(), api)
+  return loadOffers(dispatch, jest.fn(), api)
     .then(() => {
       expect(dispatch).toHaveBeenNthCalledWith(1,
           {
-            type: ActionType.LOAD_ALL_OFFERS,
+            type: ActionType.loadOffers,
             payload: OFFER_MOCK
           });
       expect(dispatch).toHaveBeenNthCalledWith(2,
           {
-            type: ActionType.CHANGE_CITY,
+            type: ActionType.changeCity,
             payload: OFFER_MOCK.city.name
           });
     });
